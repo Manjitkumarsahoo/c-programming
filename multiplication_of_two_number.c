f8452e45ab2b5411139eff08706c0524ff59f91f
Manjit kumar sahoo <124818641+Manjitkumarsahoo@users.noreply.github.com>
2023-02-15 20:30:50 +0530

    Add files via upload

---
+++ multiplication_of_two_number.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,mul;
+    printf("enter two number");
+    scanf("%d%d",&a,&b);
+    mul=a*b;
+    printf("mul is:%d",mul);
+}
---
+++ subtraction_of_two_number.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sub;
+    printf("enter two number");
+    scanf("%d%d",&a,&b);
+    sub=a-b;
+    printf("sub is:%d",sub);
+}
