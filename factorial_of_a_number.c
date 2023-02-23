e0c49b0294a985694b29e50001253274c43de508
unknown <manjitkumarsahoo02@gmail.com>
2023-02-23 23:03:10 +0530

    Day8

---
+++ factorial_of_a_number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n,i,fact=1;
+    printf("Enter the number\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        fact=fact*i;
+    }
+    printf("Factorial of no is:%d",fact);
+    return 0;
+}
---
+++ find_the_n_fibonacci_series.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    a=0;
+    b=1;
+    c=a+b;
+    int i,n;
+    printf("Enter the number\n");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        printf("%d\n",c);
+        a=b;
+        b=c;
+        c=a+b;
+    }
+    return 0;
+}
