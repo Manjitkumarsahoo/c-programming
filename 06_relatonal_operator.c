f804d81e22f339d6b1d7eaad6c0d5f4f32cb7583
unknown <manjitkumarsahoo02@gmail.com>
2023-02-27 22:30:40 +0530

    Day2

---
+++ 06_relatonal_operator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 5;
+
+    // Equal to operator
+    if (a == b) {
+        printf("a is equal to b\n");
+    } else {
+        printf("a is not equal to b\n");
+    }
+
+    // Not equal to operator
+    if (a != b) {
+        printf("a is not equal to b\n");
+    } else {
+        printf("a is equal to b\n");
+    }
+    return 0;
+}
