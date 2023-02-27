b18e0a104c9a43ce5f3dc3268ca331f6e1a5af8b
unknown <manjitkumarsahoo02@gmail.com>
2023-02-27 23:31:19 +0530

    Day11

---
+++ 36_area_and_volume_of_a_sphere.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+float r,v,a;
+printf("Input radius of the sphere");
+scanf("%f",&r);
+v=4*3.143*r*r*r/3;
+a=4*3.143*r*r;
+printf("area=%f and volume=%f",a,v);
+}
+
---
+++ 37_area_and_perimeter_of_a_rectangle.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+float l,b;
+printf("Input length and breadth of a rectangle");
+scanf("%f%f",&l,&b);
+printf("Area=%f and perimeter=%f",l*b, 2*(l+b));
+}
